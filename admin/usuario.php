b4a73579fe5e891fc847ce90f8290c6802ab6148
Freymer Sepúlveda <117765402+FrayRyderX@users.noreply.github.com>
2023-03-22 16:28:53 -0500

    Add files via upload

--- admin/usuario.php
+++ admin/usuario.php
@@ -112,9 +112,9 @@
                     <div class="flex flex-1  flex-col md:flex-row lg:flex-row mx-2">
                         <div class="mb-2 border-solid border-gray-300 rounded border shadow-sm w-full">
                             <div class="bg-gray-200 px-2 py-3 border-solid border-gray-200 border-b">
-                                <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                                <div class="w-full md:w-1/1 px-3 mb-6 md:mb-0">
                                     <form action="" method="post">
-                                        <div class="relative">
+                                        <div class="relative md:w-1/5 ">
                                             <select
                                                 class="block appearance-none w-full bg-grey-200 border border-grey-200 text-grey-darker py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-grey"
                                                 id="grid-state" name="rol">
@@ -123,21 +123,23 @@
                                                 <option value="cliente">Clientes</option>
                                             </select>
                                             <div
-                                                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-grey-darker">
+                                                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-grey-darker ">
                                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                                     viewBox="0 0 20 20">
                                                     <path
                                                         d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                                                 </svg>
                                             </div>
+                                           
                                         </div>
-                                        <div class="relative flex justify-end">
-                                            <button type="submit">Traer</button>
+                                        <div class="flex justify-end mr-16 ">
+                                            <button type="submit" class="rounded-full bg-blue-800 hover:bg-blue-500 w-32 h-8 text-white ">Traer</button>
                                         </div>
                                     </form>
+
+                                    
                                 </div>
                             </div>
-
                             <div class="p-3">
                                 <?php
                                 if (!isset($rol) || $rol == 'cliente') {
