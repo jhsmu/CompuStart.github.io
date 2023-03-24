8f9f212a3b9b975e1c4c7e3ba26705f15f8f2574
Oswaldo Natera <117487374+Oswaldo-Natera@users.noreply.github.com>
2023-03-24 16:50:23 -0500

    Add files via upload

--- admin/otros.php
+++ admin/otros.php
@@ -1,6 +1,38 @@
 <?php
-session_start();
+    session_start();
+    require('../database/basededatos.php');
+
+//Creamos un objeto del tipo Database
+$db = new Database();
+$connection = $db->connect(); //Creamos la conexión a la BD
+
+// Cuando la conexión está establecida...
+$consulta = $connection->prepare("SELECT SUM(total) FROM venta"); // Traduzco mi petición
+$consulta->execute(); //Ejecuto mi petición
+
+$ventas = $consulta->fetch(PDO::FETCH_NUM); //Me traigo los datos que necesito
+
+$consulta2 = $connection->prepare("SELECT COUNT(*) FROM cliente"); // Traduzco mi petición
+$consulta2->execute(); //Ejecuto mi petición
+
+$usuarios = $consulta2->fetch(PDO::FETCH_NUM); //Me traigo los datos que necesito
+
+$consulta3 = $connection->prepare("SELECT COUNT(*) FROM producto"); // Traduzco mi petición
+$consulta3->execute(); //Ejecuto mi petición
+
+$productos = $consulta3->fetch(PDO::FETCH_NUM); //Me traigo los datos que necesito
+
+$consulta4 = $connection->prepare("SELECT SUM(total) FROM compra"); // Traduzco mi petición
+$consulta4 ->execute(); //Ejecuto mi petición
+
+$compras = $consulta4->fetch(PDO::FETCH_NUM); //Me traigo los datos que necesito
+
+$query = $connection->prepare("SELECT * FROM detalle_venta"); // Traduzco mi petición
+$query->execute(); //Ejecuto mi petición
+
+$detalle_venta = $query->fetchAll(PDO::FETCH_ASSOC); //Me traigo los datos que necesito
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,89 +40,130 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="tailwind,tailwindcss,tailwind css,css,starter template,free template,admin templates, admin template, admin dashboard, free tailwind templates, tailwind example">
     <!-- Css -->
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/all.css">
+            <!-- iconos en fontawesome -->
+            <script src="https://kit.fontawesome.com/4b93f520b2.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i" rel="stylesheet">
-    <!-- iconos en fontawesome -->
-    <script src="https://kit.fontawesome.com/4b93f520b2.js" crossorigin="anonymous"></script>
-    <!-- Sweetalert -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <title>Otros</title>
+    <title>Ventas</title>
 </head>
 
 <body>
-    <?php
-    require('../database/basededatos.php');
+    
+<!--Container -->
+<div class="mx-auto bg-grey-400">
+    <!--Screen-->
+    <div class="min-h-screen flex flex-col">
+        <!--Header Section Starts Here-->
+        <header class="bg-nav">
+            <?php include("../componentes/headerAdmin.php") ?>
+        </header>
+        <!--/Header-->
 
-    //Creamos un objeto del tipo Database
-    $db = new Database();
-    $connection = $db->connect(); //Creamos la conexión a la BD
-    ?>
+        <div class="flex flex-1">
+            <!--Sidebar-->
+            <aside id="sidebar" class="bg-side-nav w-1/2 md:w-1/6 lg:w-1/6 border-r border-side-nav hidden md:block lg:block">
+<!--Barra lateral-->
+                <ul class="list-reset flex flex-col">
+                    <?php include("../componentes/barralateralAdmin.php") ?>
+                </ul>
+            </aside>
 
-    <!--Container -->
-    <div class="mx-auto bg-gray-300est">
-        <!--Screen-->
-        <div class="min-h-screen flex flex-col">
-            <!--Header Section Starts Here-->
-            <header class="bg-nav">
-                <?php include("../componentes/headerAdmin.php") ?>
-            </header>
+            <!--/Sidebar-->
+            <!--Main-->
+            <main class="bg-white-300 flex-1 p-3 overflow-hidden">
 
-            <div class="flex flex-1">
-                <!--Sidebar-->
-                <aside id="sidebar" class="bg- side-nav w-1/2 md:w-1/6 lg:w-1/6 border-r border-side-nav hidden md:block lg:block">
-                    <div class="flex">
 
-                    </div>
-                    <ul class="list-reset flex flex-col">
-                        <?php include("../componentes/barralateralAdmin.php") ?>
-                    </ul>
 
-                </aside>
-                <!--/Sidebar-->
-                <!--Main-->
-                <main class="bg-white-medium flex-1 p-3 overflow-hidden">
-                    <div class='flex flex-col'>
-                        <div class='flex flex-1  flex-col md:flex-row lg:flex-row mx-2'>
+                    <!-- /Stats Row Ends Here -->
 
-                            <div class='flex flex-1  flex-col md:flex-row lg:flex-row mx-2'>
-                                </div>
-                                <div class="mb-2 mx-2 border-solid border-gray-300  rounded border shadow-sm w-full md:w-1/2 lg:w-1/2">
-                                    <div class="bg-gray-200 px-2 py-3 border-solid border-gray-300 border-b">
-                                        Agregar Imagen
-                                    </div>
-                                    <div class="p-3">
-                                        <input class="modal-trigger bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="file">
-                                    </div>
+                    <!-- Card Section Starts Here -->
+                    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2">
 
-                                </div>
+                        <!-- card -->
+
+                        <div class="rounded overflow-hidden shadow bg-white mx-2 w-full">
+                            <div class="px-6 py-2 border-b border-light-grey">
+                                <div class="font-bold text-xl">Pedidos</div>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table text-grey-darkest">
+                                    <thead class="bg-grey-dark text-white text-normal">
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">ID_Venta</th>
+                                            <th scope="col">ID_Producto</th>
+                                            <th scope="col">Cantidad</th>
+                                            <th scope="col">Valor Unitario</th>
+                                            <th scope="col">Valor Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            foreach ($detalle_venta as $key => $detalle) {
+                                        ?>
+                                        <tr>
+                                            <th scope="row">                                                    
+                                                <button class="bg-blue-500 hover:bg-blue-800 text-white font-light py-1 px-2 rounded-full">
+                                                    <?php echo $detalle["id_detalle_venta"] . "<br>"; ?>
+                                                </button></th>
+                                            <td>
+                                                <button class="bg-blue-500 hover:bg-blue-800 text-white font-light py-1 px-2 rounded-full">
+                                                    <?php echo $detalle["id_venta"] . "<br>"; ?></td>
+                                                </button>
+                                            <td>
+                                                <button class="bg-blue-500 hover:bg-blue-800 text-white font-light py-1 px-2 rounded-full">
+                                                    <?php echo $detalle["id_producto"] . "<br>"; ?></td>
+                                                </button>
+                                            <td>
+                                                <button class="bg-blue-500 hover:bg-blue-800 text-white font-light py-1 px-2 rounded-full">
+                                                    <?php echo $detalle["cantidad_venta"] . "<br>"; ?>
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button class="bg-blue-500 hover:bg-blue-800 text-white font-light py-1 px-2 rounded-full">
+                                                    $ <?php echo $detalle["precio_producto"] . "<br>"; ?>
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button class="bg-blue-500 hover:bg-blue-800 text-white font-light py-1 px-2 rounded-full">
+                                                    $ <?php echo $detalle["monto_total"] . "<br>"; ?>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
-                </main>
-                <!--/Main-->
-            </div>
+                </div>
+            </main>
+            <!--/Main-->
         </div>
-
     </div>
 
-    <script src="../js/validaciones.js"></script>
-    <script src="../js/main.js"></script>
-
+</div>
+<script src="../js/main.js"></script>
 </body>
 
 </html>
+
 <?php
-if (isset($_SESSION['proveedor'])) {
+if (isset($_SESSION['actualizar_datos'])) {
     echo "<script>
     Swal.fire({
         icon: 'success',
         title: 'Éxito',
-        text: 'Proveedor agregado'
+        text: 'Datos Actualizados'
         });
     </script>";
-    unset($_SESSION['proveedor']);
+    unset($_SESSION['actualizar_datos']);
 }
 ?>
