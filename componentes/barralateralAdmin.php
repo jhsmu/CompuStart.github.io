68712ea6a419c4690d3884df5e11d9f0f5ffeda9
Oswaldo Natera <117487374+Oswaldo-Natera@users.noreply.github.com>
2023-03-24 16:48:05 -0500

    cambio de componentes

--- componentes/barralateralAdmin.php
+++ componentes/barralateralAdmin.php
@@ -48,9 +48,16 @@
                         </a>
                     </li>
                     <li class="w-full h-full py-3 px-2 border-b border-300-border bg-white">
+                        <a href="../admin/compraProveedor.php" class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
+                            <i class="fas fa-square-full float-left mx-2"></i>
+                            Compras
+                            <span><i class="fa fa-angle-right float-right"></i></span>
+                        </a>
+                    </li>
+                    <li class="w-full h-full py-3 px-2 border-b border-300-border bg-white">
                         <a href="../admin/otros.php" class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
                             <i class="fas fa-square-full float-left mx-2"></i>
-                            Otros
+                            Solicitud de pepido
                             <span><i class="fa fa-angle-right float-right"></i></span>
                         </a>
                     </li>
