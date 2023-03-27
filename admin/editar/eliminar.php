11d424474777d21d05db8a67b00f821f587b6956
Freymer Sepúlveda <117765402+FrayRyderX@users.noreply.github.com>
2023-03-27 09:14:29 -0500

    arreglado error

--- admin/editar/eliminar.php
+++ admin/editar/eliminar.php
@@ -2,17 +2,17 @@
     $id=$_GET['id'];
     include("conexion.php");
 
-    $sql="delete from administrador where id='".$id."'";
+    $sql="delete from administrador where id_administrador='".$id."'";
     $resultado=mysqli_query($conexion,$sql);
 
     if($resultado){
-        echo "<script languaje='JavaScript'>
-            alert('Los datos fueron eliminados exitosamente');
-            location.assign('../usuario.php');</script>";
+        session_start();
+        $_SESSION['eliminar_admi'] = 'registro';
+        header("location: ../usuario.php");
     } else{
         echo "<script languaje='JavaScript'>
             alert('Los datos fueron NO se eliminaron');
-            location.assign('../usuario.php');</script>";
+            location.assign('./usuario.php');</script>";
     }
 
 
