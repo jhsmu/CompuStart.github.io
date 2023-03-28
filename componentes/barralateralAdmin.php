95cdd178aacd65301480ec9144a0e5448282fa42
Freymer Sepúlveda <117765402+FrayRyderX@users.noreply.github.com>
2023-03-27 23:11:22 -0500

    DIOS AYUDAME

--- componentes/barralateralAdmin.php
+++ componentes/barralateralAdmin.php
@@ -54,7 +54,7 @@
                         </a>
                     </li>
                     <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="" class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
+                        <a href="../admin/categoria.php" class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
                             <i class="fas fa-table float-left mx-2"></i>
                             Categorias
                             <span><i class="fa fa-angle-right float-right"></i></span>
