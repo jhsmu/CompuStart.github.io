42d042b9fe9ac764eee1e536fd76a62bcbe08950
Lean <117766272+xLeaan@users.noreply.github.com>
2023-03-28 14:46:32 -0500

    Update compraProveedor.php
    ortografía

--- admin/compraProveedor.php
+++ admin/compraProveedor.php
@@ -236,7 +236,7 @@ if (isset($_POST["producto"])) {
                         <div
                             class="mb-2 md:mx-2 lg:mx-2 border-solid border-gray-200 rounded border shadow-sm w-full md:w-1/2 lg:w-1/2">
                             <div class="bg-gray-200 px-2 py-3 border-solid border-gray-200 border-b">
-                                Informacion
+                                Información
                             </div>
                             <div class="p-3">
                                 <form action="../compra/pagarCompraProveedor.php" method="post" class="w-full">
