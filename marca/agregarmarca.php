36bc0c7e23330e94f6605bec250da5972b925877
Freymer Sepúlveda <117765402+FrayRyderX@users.noreply.github.com>
2023-03-27 23:15:08 -0500

    Funcional

---
+++ marca/agregarmarca.php
@@ -0,0 +1,20 @@
+<?php
+    error_reporting( ~E_NOTICE ); // avoid notice
+	
+	require_once '../database/conexion.php';
+
+    $marca=$_POST["marca"];
+    $estado = 1;
+
+    $agregar=$DB_con->prepare('INSERT INTO marca(marca,estado_marca) VALUES(:marca, :estado_marca)');
+    $agregar->bindParam(':marca', $marca);
+    $agregar->bindParam(':estado_marca', $estado);
+
+    if ($agregar->execute()) {
+        session_start();
+        $_SESSION['marca'] = 'registro';
+        header("location: ../admin/marca.php");
+    } else {
+        echo '<script> alert("registro incorrecto")</script>';
+        header("location:marac.php");
+    }
