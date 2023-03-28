ab77e0e6ed06e2968af2432a7c20106f2f17bb94
Oswaldo Natera <oswaldonateram@gmail.com>
2023-03-28 14:10:31 -0500

    Validacion pagina

--- validaciones/sesionStarted.php
+++ validaciones/sesionStarted.php
@@ -2,6 +2,6 @@
  if(empty($_SESSION['administrador']) or empty($_SESSION['usuario'])){
   session_start();
   $_SESSION['IngresoForsozo'] = 'ingreso';
-  header("location: ../login-registro.php");
+  header("location: ../Compu_start/login-registro.php");
  }
 ?>
