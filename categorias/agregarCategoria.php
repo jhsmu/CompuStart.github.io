025f5762bd09b7d5e45860bbfce2c403c6accec4
Freymer Sepúlveda <117765402+FrayRyderX@users.noreply.github.com>
2023-03-27 23:13:57 -0500

    asdfgg

--- categorias/agregarCategoria.php
+++ categorias/agregarCategoria.php
@@ -4,15 +4,17 @@
 	require_once '../database/conexion.php';
 
     $categoria=$_POST["categoria"];
+    $estado = 1;
 
-    $agregar=$DB_con->prepare('INSERT INTO categoria(categoria) VALUES(:categoria)');
+    $agregar=$DB_con->prepare('INSERT INTO categoria(categoria,estado_categoria) VALUES(:categoria, :estado_categoria)');
     $agregar->bindParam(':categoria', $categoria);
+    $agregar->bindParam(':estado_categoria', $estado);
 
     if ($agregar->execute()) {
-        echo '<script> confirm("registro correcto")</script>';
-        header("location:categoria.php");
+        session_start();
+        $_SESSION['categoria'] = 'registro';
+        header("location: ../admin/categoria.php");
     } else {
         echo '<script> alert("registro incorrecto")</script>';
         header("location:categoria.php");
     }
-    
