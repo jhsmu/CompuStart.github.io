6649119e2d312daefc7cd91ef87708dae802441b
Freymer Sepúlveda <frayryderx@gmail.com>
2023-04-01 10:37:56 -0500

    ,.mnjhbg

--- categorias/agregarCategoria.php
+++ categorias/agregarCategoria.php
@@ -1,6 +1,6 @@
 <?php
     error_reporting( ~E_NOTICE ); // avoid notice
-	
+
 	require_once '../database/conexion.php';
 
     $consulta=$DB_con->prepare('SELECT categoria FROM categoria');
@@ -9,15 +9,15 @@
 
     $estado = 1;
 
-    foreach ($categorias as $key => $a) {
+    foreach ($categorias as $key => $nombre) {
         $categoria = "";
-        if ($_POST['categoria'] === $a['categoria']) {
+        if ($_POST['categoria'] === $nombre['categoria']) {
             session_start();
             $_SESSION["categoriaRepetida"] = "categoria repetida";
             header('location:../admin/categoria.php');
             break;
-        } else{
-            $categoria = $_POST['categoria'];
+        } else {
+            $categoria =$_POST["categoria"];
             if (isset($categoria)){
                 $agregar=$DB_con->prepare('INSERT INTO categoria(categoria,estado_categoria) VALUES(:categoria, :estado_categoria)');
                 $agregar->bindParam(':categoria', $categoria);
@@ -35,7 +35,7 @@
                         header("location: ../admin/categoria.php");
                         break;  
                     }
-                } catch (\Throwable $th) {
+                } catch (\Throwable $td) {
                     session_start();
                     $_SESSION["categoriaRepetida"] = "categoria repetida";
                     header('location:../admin/categoria.php');
@@ -43,5 +43,6 @@
                 }
                 break;
             }
+            break;
         }
     }
