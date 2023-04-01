e9fc4592958d6033424081d65df6609a5457e831
Miguel Zapata <maxmo968@gmail.com>
2023-03-31 22:49:06 -0500

    yamen

--- js/validaciones.js
+++ js/validaciones.js
@@ -142,9 +142,9 @@ function ValidacionCorreo() {
 //Nombres con números incluidos : nombre
 function NombresNumeros(){
   let nombre = document.getElementById("nombre").value;
-  let RegEx = /^[A-ZÑa-zñáéíóúÁÉÍÓÚ].[0-9A-ZÑa-zñáéíóúÁÉÍÓÚ'° -]+$/g;
+  let RegEx = /^[A-ZÑa-zñáéíóúÁÉÍÓÚ][0-9A-ZÑa-zñáéíóúÁÉÍÓÚ'° -]+$/s;
 
-  if(nombre.length < 60)
+  if(nombre.length < 60 )
     if (RegEx.test(nombre) == true) {
       document.getElementById("nombre").style.borderColor = "#008000";
     } else {
