7462b5ba3bc559618ad2952d3bd544df5ce6e612
Lean <117766272+xLeaan@users.noreply.github.com>
2023-04-03 02:05:02 -0500

    Update agregarClientes.php
    no permite correo, telefono ni cedula duplicados

--- validaciones/agregarClientes.php
+++ validaciones/agregarClientes.php
@@ -6,7 +6,7 @@ $consulta=$DB_con->prepare('SELECT email FROM cliente');
 $consulta->execute();
 $emails=$consulta->fetchAll(PDO::FETCH_ASSOC);
 
-
+// rowCount()
 if (isset($_POST["crear"])) {
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
@@ -16,49 +16,58 @@ if (isset($_POST["crear"])) {
     $documento = $_POST["numero_documento"];
     $tipo = $_POST["tipo_documento"];
     $contrasena = (htmlentities($_POST["clave"]));
+    $email = $_POST["email_registro"];
     $upload_dir = 'avatar.png';
 
-    foreach ($emails as $key => $correo) {
-        $email = "";
-        if ($_POST['email_registro'] === $correo['email']) {
-            session_start();
-            $_SESSION["emailRepetido"] = "email repetido";
-            header('location:../login-registro.php');
-            break;
-        }else{
-            $email = $_POST["email_registro"];
-            if (isset($contrasena) and isset($email)) {
-                $agregar = $DB_con->prepare('INSERT INTO cliente(imagen, apellido, nombre, tipo_documento, numero_documento, email, direccion, telefono, contrasenia, estado) VALUES(:imagen, :apellido, :nombre, :tipo, :documento, :email, :direccion, :telefono, :contrasenia, :estado)');
-                $agregar->bindParam(':imagen', $upload_dir);
-                $agregar->bindParam(':apellido', $apellido);
-                $agregar->bindParam(':nombre', $nombre);
-                $agregar->bindParam(':tipo', $tipo);
-                $agregar->bindParam(':documento', $documento);
-                $agregar->bindParam(':email', $email);
-                $agregar->bindParam(':direccion', $direccion);
-                $agregar->bindParam(':telefono', $telefono);
-                $agregar->bindParam(':contrasenia', $contrasena);
-                $agregar->bindParam(':estado', $estado);
+    $validar = "SELECT * FROM cliente WHERE email = '$email' ";
+    $validando = $DB_con->prepare($validar);
+    $validando->execute();
 
-                
-                try {
-                    if ($agregar->execute()) {
+    $validar1 = "SELECT * FROM cliente WHERE numero_documento = '$documento' ";
+    $validando1 = $DB_con->prepare($validar1);
+    $validando1->execute();
+
+    $validar2 = "SELECT * FROM cliente WHERE telefono = '$telefono' ";
+    $validando2 = $DB_con->prepare($validar2);
+    $validando2->execute();
+
+          if($validando->rowCount() > 0){
+            session_start();
+            $_SESSION["emailRepetido"] = "registro creado con exito";
+            header("location:../login-registro.php");
+          } else if($validando1->rowCount() > 0){
+            session_start();
+            $_SESSION["cedulaRepetida"] = "registro creado con exito";
+            header("location:../login-registro.php");
+          } else if($validando2->rowCount() > 0){
+            session_start();
+            $_SESSION["telefonoRepetido"] = "registro creado con exito";
+            header("location:../login-registro.php");
+          }else{
+
+            if (isset($contrasena) and isset($email)) {
+                $documento = $_POST["numero_documento"];
+                $email = $_POST["email_registro"];
+                $telefono = $_POST["telefono"];
+
+                $agregar = $DB_con->prepare('INSERT INTO cliente(imagen, apellido, nombre, tipo_documento, numero_documento, email, direccion, telefono, contrasenia, estado) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+                $ver = $agregar->execute([$upload_dir,$nombre,$apellido,$tipo,$documento,$email,$direccion,$telefono,$contrasena,$estado]);
+
+                try{
+                    if($ver){
                         session_start();
-                        $_SESSION["registro"] = "registro creado con exito";
+                        $_SESSION["registro"]= "registro creado con exito";
                         header("location:../login-registro.php");
-                        break;
-                    } else {
-                        echo '<script> alert("registro incorrecto")</script>';
-                        echo '<a href="../login-registro.php">Regresar al registro</a>';
-                        break;  
+                    }else{
+                        session_start();
+                        $_SESSION["registro"]= "registro creado con exito";
+                        header("location:../login-registro.php");
                     }
-                } catch (\Throwable $th) {
+                }catch(\Throwable $th){
                     echo '<script>alert("correo duplicado")</script>';
-                    echo '<a href="../login-registro.php">Regresar al registro</a>';
-                    break;
-                }
-                break;
+                     echo '<a href="../login-registro.php">Regresar al registro</a>';
+                    }
             }
+
         }
-    }
-}
+}  
